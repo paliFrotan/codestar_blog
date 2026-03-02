d571386cf3427853f51b06acc76e7473d92cd50c
paliFrotan <mspaligill@gmail.com>
2026-03-02 18:37:11 +0000

    Building POST model

--- codestar/settings.py
+++ codestar/settings.py
@@ -33,7 +33,7 @@ if os.path.isfile("env.py"):
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -97,7 +97,10 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
