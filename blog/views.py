6744d8e9da52a31dd59b1e71c62e7c79e07accd6
paliFrotan <mspaligill@gmail.com>
2026-03-03 01:20:55 +0000

    draft not in list

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Post
 # Create your views here.
+
+
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
     template_name = "post_list.html"
